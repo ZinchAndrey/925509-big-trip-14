4e6fa58d696c96ca65ae9164ea20e5547f2a70b0
ZinchAndrey <44947620+ZinchAndrey@users.noreply.github.com>
2021-04-14 21:07:21 +0300

    Добавляет наследование классам без параметров

--- src/view/sorting.js
+++ src/view/sorting.js
@@ -1,4 +1,4 @@
-import { createElement } from '../utils.js';
+import AbstractView from './abstract.js';
 
 function createSortingTemplate() {
   return `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
@@ -29,24 +29,8 @@ function createSortingTemplate() {
   </form>`;
 }
 
-export default class Sorting {
-  constructor() {
-    this._element = null;
-  }
-
+export default class Sorting extends AbstractView {
   getTemplate() {
     return createSortingTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
